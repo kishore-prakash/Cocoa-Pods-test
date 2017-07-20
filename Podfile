3a77ed351cc594d8bffa361ed0a7e57ef917062b
Kishore <kprakash@jabra.com>
2017-07-20 12:27:31 +0530

    Initial

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'TestGitPod' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+#  pod 'LibJabraSwiftBindings', :git => 'https://github.com/kishorep92/TestSDK.git'
+  pod 'LibJabraSwiftBindings', :path => '/Users/kishore/work/Lib_link_eg/LibJabraSwiftBindings'
+
+  # Pods for TestGitPod
+
+end
---
+++ TestGitPod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGitPod
+//
+//  Created by Kishore on 14/07/17.
+//  Copyright © 2017 Jabra. All rights reserved.
+//
+
+import UIKit
+//import LibJabraSwiftBindings
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
